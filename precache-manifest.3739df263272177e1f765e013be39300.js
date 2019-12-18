b2e1bf277c99f5fcf3f518ecc94e40145a747cb5
krunal-lia <krunallia18gmail.com>
2019-12-18 09:14:36 +0530

    Updates

---
+++ precache-manifest.3739df263272177e1f765e013be39300.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59230b1b029c29e7423948790dd7208e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b22ac6a58cd6bbcdf100",
+    "url": "/static/css/main.1b06d540.chunk.css"
+  },
+  {
+    "revision": "c4ea377548fceb536bee",
+    "url": "/static/js/2.2552fc10.chunk.js"
+  },
+  {
+    "revision": "b22ac6a58cd6bbcdf100",
+    "url": "/static/js/main.a1bd8f09.chunk.js"
+  },
+  {
+    "revision": "19a25d7f19f6cb00f2a0",
+    "url": "/static/js/runtime-main.525b080d.js"
+  }
+]);
